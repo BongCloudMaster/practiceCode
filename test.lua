50c5eb05c78ce57a498e95f7063f1bb506a245f8
(@Alco) itsbeenaminutedude <126247984+BongCloudMaster@users.noreply.github.com>
2025-05-03 16:52:35 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,16 @@
 print("Script Loaded!")
 
-return function(key)
+local function main(key)
     print("Key received:", key)  
     if key ~= "ligma balls" then
         error("Appreciate the ligma balls club! GRR!!") 
     end
     print("Key is correct!")
+end
+
+local key = ... -- adhesivo
+if key then
+    main(key)
+else
+    print("No key provided!")
 end
