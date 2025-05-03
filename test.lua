0afa6bfee80be9d3651c3885d2848259b25ecc33
(@Alco) itsbeenaminutedude <126247984+BongCloudMaster@users.noreply.github.com>
2025-05-03 17:33:10 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,7 @@
-print("Script Loaded!")
-
-local function main(key)
-    print("Key received:", key)  
-    if key ~= "ligma balls" then
-        error("Appreciate the ligma balls club! GRR!!") 
-    end
-    print("Key is correct!")
-end
-
-local key = ... -- adhesivo
-if key then
-    main(key)
-else
-    print("No key provided!")
+-- Compiled with roblox-ts v3.0.0
+return function(key)
+	if key ~= "ligma balls" then
+		error("Appreciate the ligma balls club! GRR!!")
+	end
+	print("RAN!!!")
 end
